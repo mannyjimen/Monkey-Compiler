1a2956a63e502e5a8eae98d7a05c21ee0b27ab5c
mannyjimen <manueljimenez0320>
2025-11-03 18:50:29 -0500

    lexer can read numbers, identifiers, keywords, and all single char tokens. pointer receivers

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,19 +7,55 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let hello = 20;
+	let goodbye = 10;
+	
+	let add = fn(x, y) {
+		x + y;
+	};
+	
+	let result = add(hello, goodbye);
+	`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "hello"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "20"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "goodbye"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "hello"},
 		{token.COMMA, ","},
+		{token.IDENT, "goodbye"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
