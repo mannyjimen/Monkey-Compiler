f1df620259c9b4d52064c5582ff1483628fdc943
mannyjimen <manueljimenez0320@gmail.com>
2025-11-04 15:42:28 -0500

    added repl

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/mannyjimen/Monkey-Compiler/lexer"
+	"github.com/mannyjimen/Monkey-Compiler/token"
+)
+
+const PROMPT = "<< "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Print(PROMPT)
+
+		scanned := scanner.Scan()
+		if !scanned { //no more user input
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
